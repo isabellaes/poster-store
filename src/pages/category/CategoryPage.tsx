c4bfa06d1355b35907e59bebebf12a50cc70cef1
isabellaes <borjessonisabella@gmail.com>
2024-04-17 15:36:22 +0200

    add mixins and style

--- src/pages/category/CategoryPage.tsx
+++ src/pages/category/CategoryPage.tsx
@@ -1,6 +1,19 @@
+import TuneIcon from "@mui/icons-material/Tune";
+import SwapVertIcon from "@mui/icons-material/SwapVert";
+
 const CategoryPage = () => {
   return (
     <div className="category-page">
+      <div className="sort-and-filter-btns">
+        <button className="filter-button">
+          <TuneIcon></TuneIcon>
+          FILTER
+        </button>
+        <button className="sort-button">
+          <SwapVertIcon></SwapVertIcon>
+          SORT
+        </button>
+      </div>
       <h1>category</h1>
     </div>
   );
