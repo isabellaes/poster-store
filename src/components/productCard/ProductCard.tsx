42beb205e8790a109e23cef0ce301dab69b774ca
isabellaes <borjessonisabella@gmail.com>
2024-04-18 12:43:19 +0200

    add productcard somponent

---
+++ src/components/productCard/ProductCard.tsx
@@ -0,0 +1,42 @@
+import { Product } from "../../utils/types";
+import { useNavigate } from "react-router-dom";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { useState } from "react";
+interface props {
+  product: Product;
+  addToCart: (product: Product, quantity: number) => void;
+}
+
+export default function ProductCard(props: props) {
+  const [quantity, setQuantity] = useState<number>(1);
+  const navigate = useNavigate();
+  return (
+    <div className="card">
+      <img
+        src={props.product.img}
+        alt="product-img"
+        onClick={() => navigate(`/product/${props.product.id}`)}
+      />
+
+      <div className="product-info">
+        <h3>{props.product.name}</h3>
+        <p>${props.product.price.toFixed(2)}</p>
+      </div>
+
+      <div className="add-to-cart-container">
+        <input
+          type="number"
+          value={quantity}
+          className="input-number"
+          onChange={(e) => setQuantity(parseInt(e.target.value))}
+        />
+        <button
+          className="add-button"
+          onClick={() => props.addToCart(props.product, quantity)}
+        >
+          <AddShoppingCartIcon></AddShoppingCartIcon>
+        </button>
+      </div>
+    </div>
+  );
+}
