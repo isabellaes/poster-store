8b6c3989df30cc15d1c2de24a115e3b1a2b701f6
isabellaes <borjessonisabella@gmail.com>
2024-11-20 19:16:06 +0100

    update login page

--- src/pages/admin/LoginPage.tsx
+++ src/pages/admin/LoginPage.tsx
@@ -1,16 +1,45 @@
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "../../redux/store";
+import { getUserToken } from "../../redux/reducer/adminAuthSlice";
+import { useState } from "react";
+import { User } from "../../utils/types";
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+  const [formData, setFormData] = useState<User>({
+    username: "",
+    password: "",
+  });
+  const dispatch = useDispatch<Dispatch>();
+
+  function logInUser() {
+    dispatch(getUserToken({ ...formData }));
+  }
+
   return (
     <div className="login-admin">
       <h2>Log in to admin poster store</h2>
 
-      <form onSubmit={() => navigate("productsoverview")}>
-        <TextField label="Username" />
-        <TextField type="password" label="Password" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          logInUser();
+        }}
+      >
+        <TextField
+          label="Username"
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.currentTarget.value })
+          }
+        />
+        <TextField
+          type="password"
+          label="Password"
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.currentTarget.value })
+          }
+        />
         <Button variant="contained" color="secondary" type="submit">
           Log in
         </Button>
