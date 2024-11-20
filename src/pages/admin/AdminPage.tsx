d4eb2358f984d9e4123d97fc6763af4b9b61e625
isabellaes <borjessonisabella@gmail.com>
2024-11-20 19:16:29 +0100

    update admin page

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -1,16 +1,32 @@
 import { Outlet } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Dispatch, State } from "../../redux/store";
+import { logOutUser } from "../../redux/reducer/adminAuthSlice";
+import { useSelector } from "react-redux";
 
 const AdminPage = () => {
+  const dispatch = useDispatch<Dispatch>();
+  const user = useSelector((state: State) => state.auth.token);
+  function logOut() {
+    dispatch(logOutUser());
+  }
+
   return (
     <div className="admin-page">
       <header className="admin-header">
         <h1>Admin Page</h1>
-        <nav>
-          <NavLink to="/admin">Log in</NavLink>
-          <NavLink to="/admin/productsoverview">products</NavLink>
-          <NavLink to="/admin/statistics">statistics</NavLink>
-        </nav>
+        {user ? (
+          <>
+            <nav>
+              <NavLink to="/admin">products</NavLink>
+              <NavLink to="/admin/statistics">statistics</NavLink>
+            </nav>
+            <button onClick={() => logOut()}>Log out</button>
+          </>
+        ) : (
+          <></>
+        )}
       </header>
       <div className="outlet">
         <Outlet />
